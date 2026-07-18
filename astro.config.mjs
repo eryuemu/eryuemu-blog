97b9807d551b0b6dadde15bb2351861b55f6070e
eryuemu <eryuemu@users.noreply.github.com>
2026-07-19 01:23:31 +0800

    config:

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://eryuemu.github.io',
+	base: '/eryuemu-blog',
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		shikiConfig: {
