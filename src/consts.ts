1c064566764ce446a3358670a5d9b68aff4bfd34
eryuemu <eryuemu@users.noreply.github.com>
2026-07-18 16:30:38 +0800

    feat: integrate Waline comments component

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,5 @@
 
 export const SITE_TITLE = "eryuemu's blog";
 export const SITE_DESCRIPTION = '自动化 · 嵌入式 · 系统安全';
+export const WALINE_SERVER_URL = 'https://comments.eryuemu.com'; // 请在此处填入您的 Waline 服务端地址，如果没有域名可以暂时填 Vercel 分配的域名
+
