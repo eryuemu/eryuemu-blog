67ac3b07b3a21bcf586739227fa0dedc0ac77287
eryuemu <eryuemu@users.noreply.github.com>
2026-07-28 09:36:53 +0800

    feat(friends): add Jaffrez's Blog friend link

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -11,5 +11,11 @@ export const friends: Friend[] = [
     link: 'https://111620.xyz/',
     logo: '/friends/koudai-share.png',
     desc: '所谓过往，皆为序章。虚室生白️',
+  },
+  {
+    title: "Jaffrez's Blog",
+    link: 'http://jaffrez.io',
+    logo: 'http://jaffrez.io/avatar.jpg',
+    desc: '一名热衷于计算机的学生。',
   }
 ];
