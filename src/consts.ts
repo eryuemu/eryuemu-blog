969bc40a9493ca11ba9d2eb26d68358b2c5665ec
eryuemu <eryuemu@users.noreply.github.com>
2026-08-14 14:27:56 +0800

    feat: 优化SEO描述及增加JSON-LD结构化数据(WebSite & Navigation)

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "eryuemu's blog";
-export const SITE_DESCRIPTION = '自动化 · 嵌入式 · 系统安全';
+export const SITE_DESCRIPTION = '二月木的个人博客 — 建站折腾 · AI 工具 · Galgame';
 export const WALINE_SERVER_URL = 'https://comments.eryuemu.com'; // 请在此处填入您的 Waline 服务端地址，如果没有域名可以暂时填 Vercel 分配的域名
 
